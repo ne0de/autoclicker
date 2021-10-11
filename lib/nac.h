4e977c797246122be6475d17d600ac329409b6f5
ne0de <nani.andres@hotmail.com.ar>
2021-10-11 00:20:02 -0300

    Update nac.h

--- lib/nac.h
+++ lib/nac.h
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <windows.h>
 
-// numpad: 1, 2, 3
+// numpad: 1, 2, 3, 4
 #define KEY_START 0x61
 #define KEY_STOP 0x62
-#define KEY_SAVE 0x63s
+#define KEY_SAVE 0x63
+#define KEY_CONS 0x64
 #define KEY_EXIT 0x1B
-
 #define KEY_DELAY 300
-#define CLICK_DELAY 100
 
 namespace nac
 {
@@ -18,14 +17,17 @@ namespace nac
     private:
         int x;
         int y;
+        int click_delay;
         bool running;
         bool clicking;
 
     public:
         HNAC();
-        int GetXCoord();
-        int GetYCoord();
-        void Initialize();
+        void Click(void);
+        void ShowMenu(void);
+        void Initialize(void);
+        void SetClickSpeed(void);
+        void SaveMousePosition(void);
         void SetCoordinates(int x_, int y_);
         //~HNAC();
     };
